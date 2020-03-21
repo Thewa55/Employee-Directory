32622a92ad1de712e96c28ee82e0e24adc14c21a
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-21 00:21:16 -0400

    Made employees json and started the code with other starter code to see it render properly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    employees
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employees</Title>
+        {this.state.employees.map(employee => (
+          <FriendCard
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            occupation={employee.role}
+            location={employee.email}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
